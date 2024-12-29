fcb4474954afcd3a3a85e2540cc1a5aff41f2a61
Hashem S. Ampatua <ariya.peyn01@gmail.com>
2024-12-30 07:09:57 +0800

    Created Logout Button (#9)

--- src/routes/PageHeader.svelte
+++ src/routes/PageHeader.svelte
@@ -1,3 +1,22 @@
+<script lang="ts">
+
+let showDialog = false;
+	
+const logout = () => {   
+	localStorage.removeItem('authToken');  
+	window.location.href = '/login';
+ };
+
+const openDialog = () => {
+    showDialog = true;
+};
+
+
+const closeDialog = () => {
+    showDialog = false;
+};
+
+</script>
 <header>
 	<nav class=" mx-4">
 		<ul class="flex items-center rounded-lg justify-between">
@@ -9,18 +28,28 @@
 
 
 			<!-- Avatar aligned to the end -->
-			<!-- <div class="ml-auto mr-3">
-				{#if $userStore && $userStore.user}
-					<button on:click={Dialog}> <Avatar src={ProfileSample} size="sm"/></button>
-					{#if isOpens}
-					<span class="absolute bg-white p-4 rounded shadow-md mt-2">
-						<p>Welcome, {$userStore.user.firstName}</p>
-					</span>
-					{/if}
-				{/if}
-			</div> -->
+			<div class="ml-auto mr-3">
+				<button on:click={openDialog} class="bg-red-500 text-white py-1 px-2 rounded-3xl">
+					<i class="fa-solid fa-power-off"></i>
+				</button>
+			</div>
 		</ul>
 	</nav>
+	{#if showDialog}
+		<div class="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
+			<div class="bg-white rounded-lg p-6 w-1/3">
+			<h3 class="text-lg font-semibold mb-4">Are you sure you want to logout?</h3>
+			<div class="flex justify-end gap-4">
+				<button on:click={closeDialog} class="py-1 px-3 bg-gray-500 text-white rounded-lg">
+				Cancel
+				</button>
+				<button on:click={logout} class="py-1 px-3 bg-red-500 text-white rounded-lg">
+				Logout
+				</button>
+			</div>
+			</div>
+		</div>
+    {/if}
 	<!-- <SideBar bind:isOpens={showSidebar} /> -->
 </header>
 <style>
